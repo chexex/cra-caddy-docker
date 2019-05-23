ee3693257204d96dd969cb7f0ad4ad5ec7cce24a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-23 16:56:09 +0000

    Deploy chexex/cra-caddy-docker to github.com/chexex/cra-caddy-docker.git:gh-pages

---
+++ precache-manifest.e01039a83ab854a3d01acc8cc027c522.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7b32c3e57d499eea2fced4b8a836a86",
+    "url": "/cra-caddy-docker/index.html"
+  },
+  {
+    "revision": "bc52abb4eac4354d9f13",
+    "url": "/cra-caddy-docker/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/cra-caddy-docker/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "bc52abb4eac4354d9f13",
+    "url": "/cra-caddy-docker/static/js/main.e43f2ad0.chunk.js"
+  },
+  {
+    "revision": "e2dbbabd34cdfeb9ccb8",
+    "url": "/cra-caddy-docker/static/js/runtime~main.9cfec011.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cra-caddy-docker/static/media/logo.5d5d9eef.svg"
+  }
+]);
